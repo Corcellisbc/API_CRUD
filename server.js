6fcd93990da7998becd994833e77ea54823498c0
Corcellisbc <guilhermecorcelli.sbc@gmail.com>
2024-09-26 12:32:37 -0300

    Adicionando func json() para garantir que o corpo da requisição seja interpretado como JSON

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 // Inicializando a aplicação Express
 const app = express();
 
+app.use(express.json()); // Isso garante que o corpo da requisição seja interpretado como JSON
+
 // Importando as rotas da aplicação relacionadas às tarefas
 const tarefasRoutes = require("./routes/tarefaRoutes");
 
