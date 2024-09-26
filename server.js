558b38de2c6fad201020be77572fdb2f941631ed
Corcellisbc <guilhermecorcelli.sbc@gmail.com>
2024-09-26 12:07:43 -0300

    Configurando a Aplicação Principal

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const express = require("express");
 // Inicializando a aplicação Express
 const app = express();
 
+// Importando as rotas da aplicação relacionadas às tarefas
+const tarefasRoutes = require("./routes/tarefaRoutes");
+
+// Configurando as rotas da aplicação para utilizar as rotas
+//relacionadas às tarefas
+app.use("/tarefas", tarefasRoutes);
+
 // Definindo a porta em que o servidor irá escutar
 const PORT = process.env.PORT || 3000;
 
