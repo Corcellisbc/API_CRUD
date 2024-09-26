73514f23d453d5b933530222a5f6e9276fd555c7
Corcellisbc <guilhermecorcelli.sbc@gmail.com>
2024-09-26 11:23:06 -0300

    Definindo Rotas e Controladores

---
+++ controllers/tarefaController.js
@@ -0,0 +1,46 @@
+// Controlador para lidar com operações relacioandas às tarefas
+
+//Array simulando uma lista de tarefas
+let tarefas = [];
+
+//Função para listar tarefas
+const listarTarefas = (req, res) => {
+  res.json(tarefas); // Retorna a lista de tarefas como JSON
+};
+
+// Função para criar uma nova tarefa
+const criarTarefa = (req, res) => {
+  const { descricao } = req.body; // Obtém a descrição da nova tarefa do corpo da requisição
+  const novaTarefa = { id: tarefas.length + 1, descricao }; // cria um objeto representando a nova tarefa
+  tarefas.push(novaTarefa); // Adiciona a nova tarefa à lista de tarefas
+  res.status(201).json(novaTarefa); // Retorna a nova tarefa como JSON, com o status 201 (created)
+};
+
+//Função para atualizar uma taarefa existente
+const atualizarTarefa = (req, res) => {
+  const { id } = req.params; // Ombtém o ID da tarefa a ser atualizada dos parâmetros da URL
+  const { descricao } = req.body; //Obtém a nova descrição da tarefa do corpo da requisição
+  const index = tarefas.findIndex((tarefa) => tarefa.id === parseInt(id)); // encontra o índice da tarefa na lista de tarefas
+  if (index !== -1) {
+    tarefas[index].descricao = descricao; // Atualiza a descrição da tarefa
+    res.json(tarefas[index]); // Retorna a tarefa atualizada como
+  } else {
+    res.status(404).json({ mensagem: "Tarefa não encontrada" }); // Retorna um erro 404 se a tarefa não for encontrada
+  }
+};
+
+// função para excluir uma tarefa
+const excluirTarefa = (req, res) => {
+  const { id } = req.params; // Obtém o ID da tarefa a ser excluída dos parâmetros da URL
+  const index = tarefas.findIndex((tarefa) => tarefa.id === parseInt(id)); // encontra o índice da tarefa na lista de tarefas
+  if (index !== -1) {
+    // verifica se a tarefa foi encontrada
+    tarefas.splice(index, 1); // Remove a tarefa da lista de tarefas
+    res.json({ mensagem: "Tarefa excluída com sucesso" }); // retorna uma mensagem de sucesso
+  } else {
+    res.status(404).json({ mensagem: "Tarefa não encontrada" }); // Retorna um erro 404 se atarefa não foi encontrada
+  }
+};
+
+// Exportando os controladores para serem utilizados em outros arquivos
+export default { listarTarefas, criarTarefa, atualizarTarefa, excluirTarefa };
